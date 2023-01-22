3dab2b96faeeda212d1711ebedb7ca93dabca1ba
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-01-22 16:52:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const btcPriceElement = document.getElementById("btc-price");
       const twitterFeedElement = document.getElementById("twitter-feed");
 
-      // Use a cryptocurrency API to fetch the latest Bitcoin price in USD
+      // some comment for future use C9
       fetch("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD")
         .then(response => response.json())
         .then(data => {
           btcPriceElement.innerText = data.USD;
         });
-      
-      // Use Twitter API to fetch the latest tweet from https://twitter.com/Bitcoin
+      // some comment for future use C10
       twitterFeedElement.innerHTML = '<a class="twitter-timeline" data-height="400" data-theme="dark" href="https://twitter.com/Bitcoin?ref_src=twsrc%5Etfw">Tweets by Bitcoin</a>';
       twttr.widgets.load();
